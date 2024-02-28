58e7cb5f28e0255c2e7195ad04d5c344ad3cc09b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-28 09:57:42 -0500

    starting

---
+++ README.md
@@ -0,0 +1,3 @@
+# cypress-pagination-tough-case
+
+> Example of a pagination that has tough challenges for e2e testing
---
+++ cypress/e2e/spec1.cy.js
@@ -0,0 +1,5 @@
+/// <reference types="cypress" />
+
+it('confirms the list has no such item', () => {
+  cy.visit('cypress/fixtures/spec1.html')
+})
